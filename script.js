0beeeb2548e40e34e97e04022dfb96951ba652be
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-12-29 16:19:18 -0300

    delete task

--- script.js
+++ script.js
@@ -15,5 +15,12 @@ document.querySelector("#add").onclick = function() {
                 </button>
             </div>
         `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++) {
+            current_tasks[i].onclick = function() {
+                this.parentNode.remove();
+            }
+        }
     }
 }
