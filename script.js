0c1307b733bb5a5a187751f28ebbf7875265b3ab
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-12-29 09:51:06 -0300

    taskname

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ document.querySelector("#add").onclick = function() {
     if(document.querySelector('#newtask input').value.length == 0) {
         alert("Please enter a task!")
     }
+    else {
+        document.querySelector('#tasks').innerHTML 
+        += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector
+                    ('#newtask input').value}
+                </span>
+            </div>
+        `;
+    }
 }
